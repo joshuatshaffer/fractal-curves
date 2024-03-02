37dbd60382e3ff12751cd27e6a0503ed32ef6c05
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-03-02 09:34:08 -0600

    Move render area to component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
-import { useEffect } from "react";
 import styles from "./App.module.scss";
-import { Arrow, ArrowMarker } from "./Arrow";
-import { ControlPoint } from "./ControlPoint";
-import { Path } from "./Path";
+import { FractalView } from "./FractalView";
 import {
   fillModeAtom,
   generatorAtom,
@@ -12,14 +9,9 @@ import {
   loadGeneratorAtom,
   maxIterationsAtom,
   normalizeViewAtom,
-  pointsAtom,
-  svgElementAtom,
-  viewSettingsAtom,
 } from "./atoms/atoms";
-import { eventXY } from "./eventXY";
 import { dragon, snowflakeSweep } from "./exampleFractalCurves";
-import { getBaseLine, getLines, scale, translate } from "./fractal";
-import { onDrag } from "./onDrag";
+import { scale, translate } from "./fractal";
 
 export function App() {
   const [generator, setGenerator] = useAtom(generatorAtom);
@@ -29,87 +21,17 @@ export function App() {
 
   const [fillMode, setFillMode] = useAtom(fillModeAtom);
 
-  const points = useAtomValue(pointsAtom);
-
-  const [viewSettings, setViewSettings] = useAtom(viewSettingsAtom);
-
-  const [svgElement, setSvgElement] = useAtom(svgElementAtom);
-
-  useEffect(() => {
-    if (!svgElement) {
-      return;
-    }
-
-    const listener = (event: WheelEvent) => {
-      event.preventDefault();
-      event.stopPropagation();
-
-      // TODO: Stay centered on the mouse position.
-      setViewSettings((prev) => ({
-        ...prev,
-        scale: prev.scale * 2 ** (event.deltaY * 0.01),
-      }));
-    };
-
-    svgElement.addEventListener("wheel", listener);
-    return () => {
-      svgElement.removeEventListener("wheel", listener);
-    };
-  }, [setViewSettings, svgElement]);
-
-  const normalizeView = useSetAtom(normalizeViewAtom);
-
-  // Normalize view on page load.
-  useEffect(() => {
-    normalizeView();
-  }, [normalizeView]);
-
   const loadGenerator = useSetAtom(loadGeneratorAtom);
 
   const [iterationsAnimation, setIterationsAnimation] = useAtom(
     iterationsAnimationAtom
   );
 
+  const normalizeView = useSetAtom(normalizeViewAtom);
+
   return (
     <>
-      <svg
-        ref={setSvgElement}
-        className={styles.view}
-        {...onDrag((event) => {
-          const start = eventXY(event);
-          return {
-            onDragMove: (event) => {
-              const current = eventXY(event);
-              const dx = current.x - start.x;
-              const dy = current.y - start.y;
-
-              setViewSettings({
-                ...viewSettings,
-                translate: {
-                  x: viewSettings.translate.x + dx,
-                  y: viewSettings.translate.y + dy,
-                },
-              });
-            },
-          };
-        })}
-      >
-        <defs>
-          <ArrowMarker />
-        </defs>
-        <Path points={points} fillMode={fillMode} />
-
-        <g className={styles.controls}>
-          <Arrow {...getBaseLine(generator)} color="#ff0000" />
-          {getLines(generator).map(({ from, to }, i) => (
-            <Arrow key={i} from={from} to={to} color="#0000ff" />
-          ))}
-          <ControlPoint index={-1} />
-          {generator.map((_, i) => (
-            <ControlPoint key={i} index={i} />
-          ))}
-        </g>
-      </svg>
+      <FractalView />
       <div className={styles.settingsContainer}>
         <details>
           <summary>Settings</summary>
