9f5ff68d223fdf954f6555cd9172072bc4eabd06
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-03-01 19:32:37 -0600

    Make settings menu look nice.

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,57 @@ import { onDrag } from "./onDrag";
 import { useHashState } from "./useHashState";
 import { ViewSettingsContextProvider } from "./viewSpace";
 
+const dragonGenerator: FractalCurveGenerator = [
+  {
+    reversed: true,
+    x: 10,
+    y: 0,
+  },
+  {
+    reversed: false,
+    x: 10,
+    y: 10,
+  },
+];
+
+function triLattice(x: number, y: number) {
+  return {
+    x: (x + y / 2) * 10,
+    y: -y * (Math.sqrt(3) / 2) * 10,
+  };
+}
+
+const snowflakeSweepGenerator: FractalCurveGenerator = [
+  {
+    ...triLattice(0, 1),
+    reversed: true,
+  },
+  {
+    ...triLattice(0, 2),
+    reversed: false,
+  },
+  {
+    ...triLattice(1, 2),
+    reversed: false,
+  },
+  {
+    ...triLattice(2, 1),
+    reversed: false,
+  },
+  {
+    ...triLattice(1, 0),
+    reversed: true,
+  },
+  {
+    ...triLattice(2, 0),
+    reversed: true,
+  },
+  {
+    ...triLattice(3, 0),
+    reversed: false,
+  },
+];
+
 /**
  * Limit the number of points drawn to avoid crashing the browser.
  */
@@ -30,18 +81,7 @@ export function App() {
   }>({
     iterations: 4,
     fillMode: false,
-    generator: [
-      {
-        reversed: true,
-        x: 10,
-        y: 0,
-      },
-      {
-        reversed: false,
-        x: 10,
-        y: 10,
-      },
-    ],
+    generator: snowflakeSweepGenerator,
   });
 
   const generator = state.generator;
@@ -105,124 +145,10 @@ export function App() {
 
   return (
     <>
-      <div>
-        <label htmlFor="iterations-input">Iterations </label>
-        <input
-          id="iterations-input"
-          type="number"
-          min={0}
-          max={maxIterations}
-          value={iterations}
-          onChange={(e) => {
-            setIterations(Number(e.target.value));
-          }}
-        />
-        <input
-          id="iterations-slider"
-          type="range"
-          min={0}
-          max={maxIterations}
-          step={0.1}
-          value={iterations}
-          onChange={(e) => {
-            setIterations(Number(e.target.value));
-          }}
-        />
-      </div>
-      <div>
-        <label htmlFor="fill-checkbox">Fill </label>
-        <input
-          id="fill-checkbox"
-          type="checkbox"
-          checked={fillMode}
-          onChange={(e) => {
-            setFillMode(e.target.checked);
-          }}
-        />
-      </div>
-      <div>
-        <div>
-          <input type="number" value={0} readOnly disabled />
-          <input type="number" value={0} readOnly disabled />
-        </div>
-        {generator.map((p, i) => (
-          <div key={i}>
-            <div>
-              <input
-                type="checkbox"
-                checked={p.reversed}
-                onChange={(e) => {
-                  setGenerator((g) =>
-                    g.map((p, j) =>
-                      i === j ? { ...p, reversed: e.target.checked } : p
-                    )
-                  );
-                }}
-              />
-              <button
-                type="button"
-                onClick={() => {
-                  setGenerator((g) => {
-                    const from = g[i - 1] ?? { x: 0, y: 0 };
-                    const to = g[i];
-
-                    const mid = scale(translate(to, from), 0.5);
-
-                    const newGenerator = [...g];
-                    newGenerator.splice(i, 0, { ...to, ...mid });
-                    return newGenerator;
-                  });
-                }}
-              >
-                +
-              </button>
-            </div>
-            <div>
-              <input
-                type="number"
-                value={p.x}
-                onChange={(e) => {
-                  setGenerator((g) =>
-                    g.map((p, j) =>
-                      i === j ? { ...p, x: Number(e.target.value) } : p
-                    )
-                  );
-                }}
-              />
-              <input
-                type="number"
-                value={p.y}
-                onChange={(e) => {
-                  setGenerator((g) =>
-                    g.map((p, j) =>
-                      i === j ? { ...p, y: Number(e.target.value) } : p
-                    )
-                  );
-                }}
-              />
-              {generator.length > 2 ? (
-                <button
-                  type="button"
-                  onClick={() => {
-                    setGenerator((g) => {
-                      const newGenerator = [...g];
-                      newGenerator.splice(i, 1);
-                      return newGenerator;
-                    });
-                  }}
-                >
-                  -
-                </button>
-              ) : null}
-            </div>
-          </div>
-        ))}
-      </div>
       <ViewSettingsContextProvider value={viewSettings}>
         <svg
           ref={setSvgElement}
           className={styles.view}
-          style={{ width: "100%", height: "100vh", touchAction: "none" }}
           {...onDrag((event) => {
             const start = eventXY(event);
             return {
@@ -245,31 +171,153 @@ export function App() {
           <defs>
             <ArrowMarker />
           </defs>
-          <Arrow {...getBaseLine(generator)} color="#ff000055" />
-          {getLines(generator).map(({ from, to }, i) => (
-            <Arrow key={i} from={from} to={to} color="#0000ff55" />
-          ))}
           <Path points={points} fillMode={fillMode} />
 
-          <ControlPoint
-            generator={generator}
-            setGenerator={setGenerator}
-            viewSettings={viewSettings}
-            setViewSettings={setViewSettings}
-            index={-1}
-          />
-          {generator.map((_, i) => (
+          <g className={styles.controls}>
+            <Arrow {...getBaseLine(generator)} color="#ff0000" />
+            {getLines(generator).map(({ from, to }, i) => (
+              <Arrow key={i} from={from} to={to} color="#0000ff" />
+            ))}
             <ControlPoint
-              key={i}
               generator={generator}
               setGenerator={setGenerator}
               viewSettings={viewSettings}
               setViewSettings={setViewSettings}
-              index={i}
+              index={-1}
             />
-          ))}
+            {generator.map((_, i) => (
+              <ControlPoint
+                key={i}
+                generator={generator}
+                setGenerator={setGenerator}
+                viewSettings={viewSettings}
+                setViewSettings={setViewSettings}
+                index={i}
+              />
+            ))}
+          </g>
         </svg>
       </ViewSettingsContextProvider>
+      <div className={styles.settingsContainer}>
+        <details>
+          <summary>Settings</summary>
+          <div>
+            <div>
+              <label htmlFor="iterations-input">Iterations </label>
+              <input
+                id="iterations-input"
+                type="number"
+                min={0}
+                max={maxIterations}
+                value={iterations}
+                onChange={(e) => {
+                  setIterations(Number(e.target.value));
+                }}
+              />
+              <input
+                id="iterations-slider"
+                type="range"
+                min={0}
+                max={maxIterations}
+                step={0.1}
+                value={iterations}
+                onChange={(e) => {
+                  setIterations(Number(e.target.value));
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor="fill-checkbox">Fill </label>
+              <input
+                id="fill-checkbox"
+                type="checkbox"
+                checked={fillMode}
+                onChange={(e) => {
+                  setFillMode(e.target.checked);
+                }}
+              />
+            </div>
+            <div>
+              <div>
+                <input type="number" value={0} readOnly disabled />
+                <input type="number" value={0} readOnly disabled />
+              </div>
+              {generator.map((p, i) => (
+                <div key={i}>
+                  <div>
+                    <input
+                      type="checkbox"
+                      checked={p.reversed}
+                      onChange={(e) => {
+                        setGenerator((g) =>
+                          g.map((p, j) =>
+                            i === j ? { ...p, reversed: e.target.checked } : p
+                          )
+                        );
+                      }}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setGenerator((g) => {
+                          const from = g[i - 1] ?? { x: 0, y: 0 };
+                          const to = g[i];
+
+                          const mid = scale(translate(to, from), 0.5);
+
+                          const newGenerator = [...g];
+                          newGenerator.splice(i, 0, { ...to, ...mid });
+                          return newGenerator;
+                        });
+                      }}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div>
+                    <input
+                      type="number"
+                      value={p.x}
+                      onChange={(e) => {
+                        setGenerator((g) =>
+                          g.map((p, j) =>
+                            i === j ? { ...p, x: Number(e.target.value) } : p
+                          )
+                        );
+                      }}
+                    />
+                    <input
+                      type="number"
+                      value={p.y}
+                      onChange={(e) => {
+                        setGenerator((g) =>
+                          g.map((p, j) =>
+                            i === j ? { ...p, y: Number(e.target.value) } : p
+                          )
+                        );
+                      }}
+                    />
+                    {generator.length > 2 ? (
+                      <button
+                        type="button"
+                        onClick={() => {
+                          setGenerator((g) => {
+                            const newGenerator = [...g];
+                            newGenerator.splice(i, 1);
+                            return newGenerator;
+                          });
+                        }}
+                      >
+                        -
+                      </button>
+                    ) : null}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </details>
+      </div>
     </>
   );
 }
