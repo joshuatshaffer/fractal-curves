71d50411c007ee6986c21d0396c062d6ec5067d3
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-03-02 10:49:08 -0600

    Simplify Sierpiński Arrowhead.

--- src/exampleFractalCurves.tsx
+++ src/exampleFractalCurves.tsx
@@ -100,40 +100,16 @@ export const sierpinskiArrowhead = {
   name: "Sierpiński Arrowhead",
   generator: [
     {
-      ...triLattice(1, 0),
-      reversed: false,
-    },
-    {
-      ...triLattice(1, 1),
-      reversed: true,
-    },
-    {
       ...triLattice(0, 2),
-      reversed: false,
-    },
-    {
-      ...triLattice(0, 3),
       reversed: true,
-    },
-    {
-      ...triLattice(1, 3),
-      reversed: false,
     },
     {
       ...triLattice(2, 2),
-      reversed: true,
-    },
-    {
-      ...triLattice(2, 1),
       reversed: false,
-    },
-    {
-      ...triLattice(3, 0),
-      reversed: true,
     },
     {
       ...triLattice(4, 0),
-      reversed: false,
+      reversed: true,
     },
   ],
 };
